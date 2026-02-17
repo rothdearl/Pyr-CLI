583c2e84898fee08c0565cb98d90ab4aec43c1f1
Rothimus <rothdearl@gmail.com>
2026-02-17 11:05:19 -0600

    Adding a README.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install required packages.
-pip3 install colorama --upgrade --user "$@"
-pip3 install python-dateutil --upgrade --user "$@"
-pip3 install requests --upgrade --user "$@"
+# Install required packages: use --break-system-packages on externally managed environments.
+pip3 install colorama --upgrade --user
+pip3 install python-dateutil --upgrade --user
+pip3 install requests --upgrade --user
--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-# Install required packages.
-#pip3 install coverage --upgrade --user "$@"
+# Default to no code coverage.
+CODE_COVERAGE=false
 
-python3 -m unittest discover -t ../ "test" -q
-#coverage run -m unittest discover -t ../ "test" -q
-#coverage report -m
-#coverage html
+# Check if --code-coverage is provided.
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --code-coverage)
+            CODE_COVERAGE=true
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$CODE_COVERAGE" = true ]; then
+    # Install required packages: use --break-system-packages on externally managed environments.
+    pip3 install coverage --upgrade --user
+
+    coverage run -m unittest discover -t ../ "test" -q
+    coverage report -m
+    coverage html
+else
+    python3 -m unittest discover -t ../ "test" -q
+fi
